292620d391250d93609f041b98e4eff6c0eb0123
deaaaldeen45112 <deaaaldeen45112@gmail.com>
2022-02-14 21:22:32 +0200

    first commit

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,7 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { IndexService } from '../services/index.service';
-
+// work
 
 @Component({
   selector: 'app-header',
